edad718ef0034f7a3be019ed2fe2959b3f1ec5d9
Brian Bacon <bacon011@connect.wcsu.edu>
2018-07-15 15:10:51 -0400

    Added error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,29 @@
 class ArticlesController < ApplicationController
-	def index
-		@articles = Article.all
-	end
+  def index
+    @articles = Article.all
+  end
 
-	def show
-		@article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 	
-	def new
-	end
+  def new
+	@article = Article.new
+  end
 	
-	def create
-		#render plain: params[:article].inspect
-		@article = Article.new(params.require(:article).permit(:title, :text))
+  def create
+	#render plain: params[:article].inspect
+	@article = Article.new(article_params)
 		
-		@article.save
-		redirect_to @article
+	if @article.save
+	  redirect_to @article
+	else
+	  render 'new'
+	end
+  end
+	
+  private
+    def article_params
+	  params.require(:article).permit(:title, :text)
 	end
 end
